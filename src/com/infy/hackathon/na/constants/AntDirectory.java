24f5193a2877a30099fb2434172f536b1bb55af0
Karthik Pissey <kpissey@cisco.com>
2018-03-02 16:29:25 +0530

    Adding the AntToMaven core engine

---
+++ src/com/infy/hackathon/na/constants/AntDirectory.java
@@ -0,0 +1,12 @@
+package com.infy.hackathon.na.constants;
+
+public enum AntDirectory {
+	
+	PROJECT_FOLDER,
+	SRC,
+	LIB,
+	WEBCONTENT,
+	TESTSRC
+	
+
+}
---
+++ src/com/infy/hackathon/na/constants/MavenFile.java
@@ -0,0 +1,12 @@
+package com.infy.hackathon.na.constants;
+
+/**
+ * 
+ * @author Nerdy_Affair
+ * Enum to maintain the directory names for Maven Project Folder
+ */
+public enum MavenFile {
+	
+	WELCOME_FILE //index.jsp
+
+}
